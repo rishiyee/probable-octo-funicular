14ab3bf4316f860989dd204ca2ad86d7a7148f8c
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-09-29 06:41:22 +0530

    update: hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Navbar from "./Navbar";
-import GlassButton from "./GlassButton";
 
 export default function Hero() {
   return (
@@ -9,21 +8,26 @@ export default function Hero() {
       <Navbar />
 
       {/* Background Image */}
-      <Image src="/hero.png" alt="Hero Background" fill priority className="object-cover" />
+      <Image
+        src="/hero.png"
+        alt="Hero Background"
+        fill
+        priority
+        className="object-cover"
+      />
 
       {/* Gradient overlay */}
       <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/90 z-10"></div>
 
       {/* Text container */}
-      <div className="relative z-20 flex h-full items-end text-left text-white px-20 2xl:px-20 gap-8 pb-20">
-        <h1 className="flex-[3] text-[126px] md:text-[96px] font-normal leading-tight">
+      <div className="relative z-20 flex h-full flex-col lg:flex-row justify-end lg:items-end text-left text-white px-4 sm:px-8 md:px-20 gap-0 lg:gap-4 pb-10 md:pb-20">
+        <h1 className="flex-none md:flex-[3] text-6xl sm:text-8xl md:text-[96px] 2xl:text-[126px] font-normal leading-snug sm:leading-normal md:leading-tight">
           Design beyond<br />Imagination
         </h1>
-        <p className="flex-[1] text-[24px] md:text-[20px] leading-snug">
+        <p className="flex-none md:flex-[1] text-base sm:text-lg md:text-[20px] lg:text-[24px] leading-snug">
           Discover amazing products and experiences tailored just for you.
         </p>
       </div>
-
     </section>
   );
 }
