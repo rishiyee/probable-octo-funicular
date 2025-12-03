1938aa0e03eb1dba19a5954182a47e2809439333
rishiyee <hrishikeshvyshnavop@gmail.com>
2025-12-03 10:10:08 +0530

    code refinement"

--- app/page.tsx
+++ app/page.tsx
@@ -2,35 +2,14 @@ import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection";
 import WorksSection from "@/components/WorksSection";
 import ScrollMarquee from "@/components/ScrollMarquee";
-import ServicesSection from "@/components/ServicesSection";
-
-
-
-
 
 export default function Home() {
   return (
     <main className="bg-black min-h-screen flex flex-col">
-
-
-
       <Hero />
       <AboutSection />
       <WorksSection />
-      <ServicesSection /> */}
-      {/* <ScrollMarquee /> */}
-
-
-
-
-
-
-
-
-
-
-
-
+      <ScrollMarquee />
     </main>
   );
 }
--- components/WorksSection.tsx
+++ components/WorksSection.tsx
@@ -3,40 +3,39 @@ import Pill from "./Pill";
 import Work from "./Work";
 
 export default function WorksSection() {
-    return (
-        <section className="w-full py-16 gap-16">
-            <Work />
-            <div className="max-w-[1440px] w-full mx-auto px-4 flex flex-col gap-16">
+  return (
+    <section className="w-full py-16 gap-16">
+      <Work />
 
-                <WorksContainer
-                    imageSrc="/project1.jpg"
-                    title="E-Commerce Website"
-                    link="https://dribbble.com/shots/1234567"
-                    rightContent={
-                        <>
-                            <Pill>UI/UX</Pill>
-                            <Pill variant="glass">Web</Pill>
-                        </>
-                    }
-                />
+      <div className="max-w-[1440px] w-full mx-auto px-4 flex flex-col gap-16">
 
-                {/* Add more works like this */}
+        <WorksContainer
+          imageSrc="/project1.jpg"
+          title="E-Commerce Website"
+          link="https://dribbble.com/shots/1234567"
+          rightContent={
+            <>
+              <Pill>UI/UX</Pill>
+              <Pill variant="glass">Web</Pill>
+            </>
+          }
+        />
 
-                <WorksContainer
-                    imageSrc="/mockup2.jpg"
-                    title="Fintech Dashboard"
-                    link="https://dribbble.com/shots/1234567"
+        {/* Add more works like this */}
 
-                    rightContent={
-                        <>
-                            <Pill>UI/UX</Pill>
-                            <Pill variant="glass">App</Pill>
-                        </>
-                    }
-                />
+        <WorksContainer
+          imageSrc="/mockup2.jpg"
+          title="Fintech Dashboard"
+          link="https://dribbble.com/shots/1234567"
+          rightContent={
+            <>
+              <Pill>UI/UX</Pill>
+              <Pill variant="glass">App</Pill>
+            </>
+          }
+        />
 
-
-            </div>
-        </section>
-    );
+      </div>
+    </section>
+  );
 }
